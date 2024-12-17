a87c6d68c5242c2e89f737e9eaf95200523f4b5c
Harsh-09A <harsh.autowebbed@gmail.com>
2024-12-17 08:21:03 +0530

    Demo Ready

--- js/script.js
+++ js/script.js
@@ -14,3 +14,26 @@ function toggleContent(dotsId, moreTextId, btnId) {
     moreText.style.display = "inline";
   }
 }
+
+var swiper = new Swiper(".testimonial-wrapper", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 30,
+  loop: true,
+  speed: 1300,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    480: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+  },
+});
